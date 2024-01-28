9d359723355f7955ee959d01df771812f63a094b
artn-dev <az-rtn@tutanota.com>
2024-01-28 17:04:34 -0300

    Delete exercises with through dropdown menu

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,10 +1,12 @@
 const exerciseListView = (req, res) => {
     //TODO: Coletar exercícios para exibição pelo modelo
     const exercises = [
-        { name: 'Agachamento', sets: 7,  reps: 13 },
-        { name: 'Exerc 1', sets: 7,  reps: 13 },
-        { name: 'Exerc 2', sets: 7,  reps: 13 },
-        { name: 'Exerc 3', sets: 7,  reps: 13 },
+        { name: 'Exerc 1', sets: 7,  reps: 13, id: 1 },
+        { name: 'Exerc 2', sets: 7,  reps: 13, id: 2 },
+        { name: 'Exerc 3', sets: 7,  reps: 13, id: 3 },
+        { name: 'Exerc 4', sets: 7,  reps: 13, id: 4 },
+        { name: 'Exerc 4', sets: 7,  reps: 13, id: 5 },
+        { name: 'Exerc 4', sets: 7,  reps: 13, id: 6 },
     ];
 
     res.render('exercises/list', {
@@ -41,7 +43,17 @@ const registerExercise = (req, res) => {
 }
 
 
+const  deleteExercise = (req, res) => {
+    const routineId = req.params.id;
+
+    // TODO: Excluir rotina da base de dados
+
+    res.send({ next_page: '/exercicios' });
+}
+
+
 module.exports = {
     exerciseListView,
-    registerExercise
+    registerExercise,
+    deleteExercise
 }
--- routes/exercises.js
+++ routes/exercises.js
@@ -6,10 +6,11 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 // const upload = multer({ dest: "uploads/" });
 
-const { exerciseListView, registerExercise } = require('../controllers/exercises.js');
+const { exerciseListView, registerExercise, deleteExercise } = require('../controllers/exercises.js');
 
 router.get('/', exerciseListView);
 router.get('/criar-exercicio', registerExercise);
 router.post('/criar-exercicio', upload.single('files'), registerExercise);
+router.delete('/excluir-exercicio/:id', deleteExercise);
 
 module.exports = router;
